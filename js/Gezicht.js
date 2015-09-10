84d9b43e3f63c0b8ac4bd11fbdcc5c7f3af28cfa
jelleM <jelle.mannaerts@gmail.com>
2015-09-10 19:51:03 +0200

    drawing the mouth

--- js/Gezicht.js
+++ js/Gezicht.js
@@ -41,33 +41,67 @@ neus.simplify(0.1);
 /* MOND */
 var gezichtbottom = gezicht.bounds.bottomCenter;
 var neusbottom = neus.bounds.bottomCenter;
-var mondhulp = new Path();
-mondhulp.add(new Point(gezichtbottom));
-mondhulp.add(new Point(neusbottom));
+var verticalLineMond = new Path();
+verticalLineMond.add(new Point(gezichtbottom));
+verticalLineMond.add(new Point(neusbottom));
 
+var gezichtLeft = gezicht.bounds.leftCenter;
+var gezichtCenter = gezicht.bounds.center;
+var horizontalLeftMond = new Path();
+horizontalLeftMond.add(new Point(gezichtLeft));
+horizontalLeftMond.add(new Point(gezichtCenter));
 
-var startpunt = new Point(mondhulp.getPointAt(mondhulp.length / 2));
-var punt2 = new Point(startpunt + new Point((Math.random() * (gezicht.bounds.width / 5) + 20), (-20 + Math.random() * 5 + 1)));
-var punt3 = new Point(startpunt - new Point((Math.random() * (gezicht.bounds.width / 5) + 20), (20 + Math.random() * 5 + 1)));
-var mond = new Path.Arc(punt2, startpunt, punt3);
+var gezichtRight = gezicht.bounds.rightCenter;
+var horizontalRightMond = new Path();
+horizontalRightMond.add(new Point(gezichtCenter));
+horizontalRightMond.add(new Point(gezichtRight));
+
+var left = new Point(horizontalLeftMond.getPointAt(horizontalLeftMond.length / 2));
+left.y += (Math.random()*30) + 5;
+
+var leftBetweenHelp = new Path();
+leftBetweenHelp.add(new Point(left));
+leftBetweenHelp.add(new Point(gezichtCenter));
+var leftTween = new Point(leftBetweenHelp.getPointAt(leftBetweenHelp.length / 2));
+leftTween.y =  left.y + (Math.random()*20) + 5;
+
+var mid = new Point(verticalLineMond.getPointAt(verticalLineMond.length / 2));
+mid.y = leftTween.y + (Math.random()*15);
+
+var right = new Point(horizontalRightMond.getPointAt(horizontalRightMond.length / 2));
+right.y = left.y;
+
+var rightBetweenHelp = new Path();
+rightBetweenHelp.add(new Point(right));
+rightBetweenHelp.add(new Point(gezichtCenter));
+var rightTween = new Point(rightBetweenHelp.getPointAt(rightBetweenHelp.length / 2));
+rightTween.y =  leftTween.y;
+
+var rightUnder = new Point(right);
+rightUnder.y += (Math.random()*20) + 5;
+var rightTweenUnder = new Point(rightTween);
+rightTweenUnder.y += (Math.random()*20) + 10;
+var midUnder = new Point(mid);
+midUnder.y = rightTweenUnder.y + (Math.random()*10) + 5;
+var leftUnder = new Point(left);
+leftUnder.y = rightUnder.y;
+var leftTweenUnder = new Point(leftTween);
+leftTweenUnder.y = rightTweenUnder.y;
+
+var mond = new Path();
+mond.add(left);
+mond.add(leftTween);
+mond.add(mid);
+mond.add(rightTween);
+mond.add(right);
+mond.add(rightUnder);
+mond.add(rightTweenUnder);
+mond.add(midUnder);
+mond.add(leftTweenUnder);
+mond.add(leftUnder);
 mond.closePath();
-mond.simplify(0.1);
+mond.simplify(0.01);
 mond.fillColor = "#818181";
-
-/* OGEN */
-var gezichttop = gezicht.bounds.topCenter;
-var ooghulp = new Path();
-ooghulp.add(new Point(gezichttop));
-ooghulp.add(new Point(neusbottom));
-var startpunt2 = new Point(ooghulp.getPointAt(ooghulp.length / 2));
-var oogafstand = ((Math.random()*25)+15);
-var oog1Hulp = new Point(startpunt2 - new Point(oogafstand,0));
-var oog2Hulp = new Point(startpunt2 + new Point(oogafstand,0));
-var ooggroote = ((Math.random()*7)+3);
-var oog1 = new Path.Circle(oog1Hulp, ooggroote);
-var oog2 = new Path.Circle(oog2Hulp, ooggroote);
-oog1.fillColor = "#818181";
-oog2.fillColor = "#818181";
 
 
 
